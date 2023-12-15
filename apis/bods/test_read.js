d5d1c92652674d5f8b52253924ed5d8261f16c08
Cyclingbob <bob21567@yahoo.com>
2023-12-15 22:05:00 +0000

    along with rename reading.js, update test_read.js as required

--- apis/bods/test_read.js
+++ apis/bods/test_read.js
@@ -2,5 +2,5 @@ const path = require("path")
 // const timetable = require("./timetable")
 var timetable = path.join(__dirname, "./timetables/25_825-FEAO025_FEAO825--FESX-Basildon-2023-10-29-For_Reports-BODS_V1_1.xml")
 
-const parse = require("./reading").readXML
+const parse = require("./read").readXML
 parse(timetable).then(console.log)
